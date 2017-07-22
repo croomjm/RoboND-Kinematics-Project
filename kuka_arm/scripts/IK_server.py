6b1d6ab006f9fab6fb4d836a5ee8456635a0b2c0
croomjm <croomjm@gmail.com>
2017-07-21 17:49:23 -0700

    Modified to pull IK model of Kuka arm from Kuka_IK class. IK_server now just runs code for interacting with the pick and place test.

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2017 Electric Movement Inc.
-#
-# This file is part of Robotic Arm: Pick and Place project for Udacity
-# Robotics nano-degree program
-#
-# All Rights Reserved.
-
-# Author: Harsh Pandya
-
 # import modules
 import rospy
 import tf
@@ -17,71 +8,61 @@ from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from geometry_msgs.msg import Pose
 from mpmath import *
 from sympy import *
+from IK import Kuka_IK
 
+class Pose_Error(object):
+    #class to record position and orientation error
+    #for each IK iteration
+    def __init__(self):
+        pass
 
-def handle_calculate_IK(req):
-    rospy.loginfo("Received %s eef-poses from the plan" % len(req.poses))
-    if len(req.poses) < 1:
-        print "No valid poses received"
-        return -1
-    else:
-        # Initialize service response
-        joint_trajectory_list = []
-        for x in xrange(0, len(req.poses)):
-            # IK code starts here
-            joint_trajectory_point = JointTrajectoryPoint()
+class handle_IK(object):
+    def __init__(self, Kuka_IK_object):
+        ik = Kuka_IK_object
 
-            # Define DH param symbols
+    def handle_calculate_IK(self, req):
+        rospy.loginfo("Received %s eef-poses from the plan" % len(req.poses))
+        if len(req.poses) < 1:
+            print "No valid poses received"
+            return -1
+        else:
+            # Initialize service response
+            joint_trajectory_list = []
+            for x in xrange(0, len(req.poses)):
+                # IK code starts here
+                joint_trajectory_point = JointTrajectoryPoint()
 
+               # px,py,pz = end-effector position
+               # roll, pitch, yaw = end-effector orientation (in gazebo frame)
+                px = req.poses[x].position.x
+                py = req.poses[x].position.y
+                pz = req.poses[x].position.z
 
-            
-            # Joint angle symbols
-
-
-      
-            # Modified DH params
-
-
-            
-            # Define Modified DH Transformation matrix
-
-
-
-            # Create individual transformation matrices
-
-
-            
-            # Extract end-effector position and orientation from request
-	    # px,py,pz = end-effector position
-	    # roll, pitch, yaw = end-effector orientation
-            px = req.poses[x].position.x
-            py = req.poses[x].position.y
-            pz = req.poses[x].position.z
-
-            (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
-                [req.poses[x].orientation.x, req.poses[x].orientation.y,
+                (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
+                    [req.poses[x].orientation.x, req.poses[x].orientation.y,
                     req.poses[x].orientation.z, req.poses[x].orientation.w])
-     
-            # Calculate joint angles using Geometric IK method
 
-		
+                # Populate response for the IK request
+                # In the next line replace theta1,theta2...,theta6 by your joint angle variables
+                joint_trajectory_point.positions = ik.calculateJointAngles(px, py, pz, roll, pitch, yaw)
+                joint_trajectory_list.append(joint_trajectory_point)
 
+            rospy.loginfo("length of Joint Trajectory List: %s" % len(joint_trajectory_list))
+            return CalculateIKResponse(joint_trajectory_list)
 
-            # Populate response for the IK request
-            # In the next line replace theta1,theta2...,theta6 by your joint angle variables
-	    joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
-	    joint_trajectory_list.append(joint_trajectory_point)
-
-        rospy.loginfo("length of Joint Trajectory List: %s" % len(joint_trajectory_list))
-        return CalculateIKResponse(joint_trajectory_list)
-
-
-def IK_server():
-    # initialize node and declare calculate_ik service
-    rospy.init_node('IK_server')
-    s = rospy.Service('calculate_ik', CalculateIK, handle_calculate_IK)
-    print "Ready to receive an IK request"
-    rospy.spin()
+    def IK_server(self):
+        # initialize node and declare calculate_ik service
+        rospy.init_node('IK_server')
+        s = rospy.Service('calculate_ik', CalculateIK, self.handle_calculate_IK)
+        print "Ready to receive an IK request"
+        rospy.spin()
 
 if __name__ == "__main__":
-    IK_server()
+    #instantiate Kuka_IK object
+    ik = Kuka_IK()
+
+    #instantiate handle_IK object
+    h = handle_IK(ik)
+
+    #run the IK server
+    h.IK_server()
